f26862731a4e42f9756140fdf31f98ecf7e46e47
Rainer Kuhn <ticktricktrack@gmail.com>
2015-03-10 13:42:09 +0000

    Move visual replace bindings to bindings file

--- config/00common.el
+++ config/00common.el
@@ -161,7 +161,3 @@
 ;; visual regexp
 (add-to-list 'load-path "folder-in-which-visual-regexp-files-are-in/") ;; if the files are not already in the load path
 (require 'visual-regexp)
-(define-key global-map (kbd "C-c r") 'vr/replace)
-(define-key global-map (kbd "C-c q") 'vr/query-replace)
-;; if you use multiple-cursors, this is for you:
-(define-key global-map (kbd "C-c m") 'vr/mc-mark)
--- config/01bindings.el
+++ config/01bindings.el
@@ -14,3 +14,9 @@
 
 ;; next window
 (define-key global-map (kbd "C-o") 'previous-multiframe-window)
+
+;; visual regexp
+(define-key global-map (kbd "C-c r") 'vr/replace)
+(define-key global-map (kbd "C-c q") 'vr/query-replace)
+;; if you use multiple-cursors, this is for you:
+(define-key global-map (kbd "C-c m") 'vr/mc-mark)
